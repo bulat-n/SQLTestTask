7b4ee7922b3669c2f992148982ffa2142bd314c9
Bulat-N <bulat@nuriev.me>
2018-07-15 20:05:12 +0300

    Добавлены файлы проекта.

---
+++ SQLTestTask/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SQLTestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(448, 180);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Название книги";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Количество со-авторов";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 180);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Библиотека";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ SQLTestTask/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLTestTask
+{
+    public partial class Form1 : Form
+    {
+        static SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=.;" +
+            "Initial Catalog=.;" +
+            "Integrated Security=SSPI;";
+            conn.Open();
+
+            // TRUNCATE TABLE AutorsTable
+            SqlCommand command = new SqlCommand("TRUNCATE TABLE dbo.Autors", conn);
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("TRUNCATE TABLE dbo.Books", conn);
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("TRUNCATE TABLE dbo.BooksAndAutors", conn);
+            command.ExecuteNonQuery();
+
+            AddAutor("ААА");
+            AddAutor("БББ");
+            AddAutor("ВВВ");
+            AddAutor("ГГГ");
+            AddAutor("ДДД");
+
+            AddBook("AAA", new List<string> { "ААА", "БББ" });
+            AddBook("BBB", new List<string> { "ВВВ" });
+            AddBook("CCC", new List<string> { "ГГГ", "ДДД" });
+            AddBook("DDD", new List<string> { "ААА" });
+            AddBook("EEE", new List<string> { "БББ", "ВВВ", "ГГГ" });
+            AddBook("FFF", new List<string> { "ДДД" });
+            AddBook("GGG", new List<string> { "ААА", "БББ", "ВВВ", "ГГГ", "ДДД" });
+            
+            var commandText =
+                @"select Book, AutorsNum
+                from (SELECT Books.Book, count(BooksAndAutors.id) as AutorsNum
+                FROM Books
+                INNER JOIN BooksAndAutors ON Books.id=BooksAndAutors.BookId
+                group by Books.id, Books.Book) m1
+                WHERE AutorsNum >= 3";
+            command = new SqlCommand(commandText, conn);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                try
+                {
+                    var Book = reader.GetString(0);
+                    var AutorsNum = reader.GetInt32(1);
+                    dataGridView1.Rows.Add(Book, AutorsNum);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+            }
+            conn.Close();
+        }
+
+        static void AddAutor(string Autor)
+        {
+            var commandText = $"INSERT INTO dbo.Autors (Autor) VALUES ('{Autor}')";
+            SqlCommand command = new SqlCommand(commandText, conn);
+            command.ExecuteNonQuery();
+        }
+
+        static void AddBook(string BookName, List<string> BookAutors)
+        {
+            var commandText = $"INSERT INTO dbo.Books (Book) VALUES ('{BookName}')";
+            SqlCommand command = new SqlCommand(commandText, conn);
+            command.ExecuteNonQuery();
+            foreach (var Autor in BookAutors)
+                AddConnection(BookName, Autor);
+        }
+
+        static void AddConnection(string BookName, string BookAutor)
+        {
+            var commandText = $"INSERT INTO dbo.BooksAndAutors (BookId, AutorId) VALUES ((SELECT Id FROM dbo.Books WHERE Book = '{BookName}'), (SELECT Id FROM dbo.Autors WHERE Autor = '{BookAutor}'))";
+            SqlCommand command = new SqlCommand(commandText, conn);
+            command.ExecuteNonQuery();
+        }
+    }
+}
